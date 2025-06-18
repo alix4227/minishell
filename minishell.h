2628f005a10ea9159fc1885b9c88cbcab2102fba
alix4227 <alix_07@hotmail.fr>
2025-06-18 19:57:01 +0200

    modif parsing file

--- minishell.h
+++ minishell.h
@@ -93,4 +93,6 @@ int		signal_handlers(t_global global);
 void	signal_handler(int signum);
 void	is_redir_start(t_data * data);
 int		is_error(char *args);
+int	is_error_2(t_data *data, t_list *list);
+int	is_unclosed_quotes(char *args);
 #endif
